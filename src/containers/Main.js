75a61963f7c5f3c480c0c000253755256c6ec19e
Muhtasim Fuad Rafid <fuadrafid.dev@gmail.com>
2024-02-28 21:59:56 -0700

    fix imports

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -8,7 +8,6 @@ import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
-import Footer from "../components/footer/Footer";
 import Talks from "./talks/Talks";
 import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
@@ -22,55 +21,54 @@ import {useLocalStorage} from "../hooks/useLocalStorage";
 import "./Main.scss";
 
 const Main = () => {
-  const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
-  const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
-    useState(true);
+    const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+    const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+    const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
+        useState(true);
 
-  useEffect(() => {
-    if (splashScreen.enabled) {
-      const splashTimer = setTimeout(
-        () => setIsShowingSplashAnimation(false),
-        splashScreen.duration
-      );
-      return () => {
-        clearTimeout(splashTimer);
-      };
-    }
-  }, []);
+    useEffect(() => {
+        if (splashScreen.enabled) {
+            const splashTimer = setTimeout(
+                () => setIsShowingSplashAnimation(false),
+                splashScreen.duration
+            );
+            return () => {
+                clearTimeout(splashTimer);
+            };
+        }
+    }, []);
 
-  const changeTheme = () => {
-    setIsDark(!isDark);
-  };
+    const changeTheme = () => {
+        setIsDark(!isDark);
+    };
 
-  return (
-    <div className={isDark ? "dark-mode" : null}>
-      <StyleProvider value={{isDark: isDark, changeTheme: changeTheme}}>
-        {isShowingSplashAnimation && splashScreen.enabled ? (
-          <SplashScreen />
-        ) : (
-          <>
-            <Header />
-            <Greeting />
-            <Skills />
-            <StackProgress />
-            <Education />
-            <WorkExperience />
-            <Projects />
-            <StartupProject />
-            <Achievement />
-            <Blogs />
-            <Talks />
-            <Twitter />
-            <Podcast />
-            <Profile />
-            {/*<Footer />*/}
-            <ScrollToTopButton />
-          </>
-        )}
-      </StyleProvider>
-    </div>
-  );
+    return (
+        <div className={isDark ? "dark-mode" : null}>
+            <StyleProvider value={{isDark: isDark, changeTheme: changeTheme}}>
+                {isShowingSplashAnimation && splashScreen.enabled ? (
+                    <SplashScreen/>
+                ) : (
+                    <>
+                        <Header/>
+                        <Greeting/>
+                        <Skills/>
+                        <StackProgress/>
+                        <Education/>
+                        <WorkExperience/>
+                        <Projects/>
+                        <StartupProject/>
+                        <Achievement/>
+                        <Blogs/>
+                        <Talks/>
+                        <Twitter/>
+                        <Podcast/>
+                        <Profile/>
+                        <ScrollToTopButton/>
+                    </>
+                )}
+            </StyleProvider>
+        </div>
+    );
 };
 
 export default Main;
